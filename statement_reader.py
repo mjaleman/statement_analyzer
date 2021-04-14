e05739b92b36e6698c292ec6454ff78643639609
Marlon Aleman <mjaleman1997@gmail.com>
2021-04-14 00:15:15 -0700

    Add comments for readability

--- statement_reader.py
+++ statement_reader.py
@@ -4,12 +4,14 @@ import decimal
 categories = {}
 total = 0
 
+# Add key value pairs to dictionary.
 def organize_into_dict(category, amount):
     if category not in categories:
         categories[category] = amount
     else:
         categories[category] += amount
 
+# Reads CSV files as a dictionary
 with open('Discover-Statement-20210412.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     line_count = 0
@@ -17,11 +19,13 @@ with open('Discover-Statement-20210412.csv') as csv_file:
         if line_count == 0:
             line_count += 1
             pass
+        # Converts str to decimal value
         string_to_dec = decimal.Decimal(row["Amount"])
         organize_into_dict(row["Category"], string_to_dec)
         total += string_to_dec
         line_count += 1
 
+# Remove payments to prevent errors with pie chart
 del categories["Payments and Credits"]
 
 for key in categories:
